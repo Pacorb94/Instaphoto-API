53a1df677b457a99946d7572f37434f9fa96b7aa
Pacorb94 <a@a.com>
2022-01-17 20:12:07 +0100

    CHANGE CreateUsersTable migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -15,12 +16,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('role')->nullable(false);
+            $table->string('nick')->unique()->nullable(false);
+            $table->string('email')->unique()->nullable(false);
+            $table->string('password')->nullable(false);
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->primary('id');
         });
     }
 
