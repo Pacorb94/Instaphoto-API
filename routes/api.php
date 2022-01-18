3685397eee01b8e8be319e4bbcd1e49be37c3993
Pacorb94 <a@a.com>
2022-01-18 05:45:20 +0100

    CHANGE api.php

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::post('/login', 'App\Http\Controllers\UserController@login');
 
 Route::middleware('auth:sanctum')->group(function () {
     //User
-    Route::post('/users/{id}/profile-image', 'App\Http\Controllers\UserController@uploadProfileImage');
+    Route::post('/users/{user}/profile-image', 'App\Http\Controllers\UserController@uploadProfileImage');
     Route::get(
         '/users/profile-images/{imageName}',
         'App\Http\Controllers\UserController@getProfileImage'
@@ -31,15 +31,15 @@ Route::middleware('auth:sanctum')->group(function () {
     //Image
     Route::post('/image', 'App\Http\Controllers\ImageController@upload');
     Route::get('/images', 'App\Http\Controllers\HomeController@getImages');
-    Route::put('/images/{id}', 'App\Http\Controllers\ImageController@update');
-    Route::delete('/images/{id}', 'App\Http\Controllers\ImageController@delete');
+    Route::put('/images/{image}', 'App\Http\Controllers\ImageController@update');
+    Route::delete('/images/{image}', 'App\Http\Controllers\ImageController@delete');
 
     //Comment
-    Route::post('/images/{imageId}/comment', 'App\Http\Controllers\CommentController@create');
-    Route::delete('/comments/{id}', 'App\Http\Controllers\CommentController@delete');
+    Route::post('/images/{image}/comment', 'App\Http\Controllers\CommentController@create');
+    Route::delete('/comments/{comment}', 'App\Http\Controllers\CommentController@delete');
 
     //Like
-    Route::post('/images/{imageId}/like', 'App\Http\Controllers\LikeController@giveLike');
+    Route::post('/images/{image}/like', 'App\Http\Controllers\LikeController@giveLike');
     Route::get('/likes', 'App\Http\Controllers\LikeController@getLikes');
-    Route::delete('/images/{imageId}/dislike', 'App\Http\Controllers\LikeController@giveDislike');
+    Route::delete('/images/{image}/dislike', 'App\Http\Controllers\LikeController@giveDislike');
 });
