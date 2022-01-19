10dd77a5895951cb5b9f73011da03055c9b679e2
Pacorb94 <a@a.com>
2022-01-19 04:12:10 +0100

    CHANGE LoginRequest

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -25,7 +25,7 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|exists:users,email',
+            'email' => 'required|email|exists:users,email',
             'password' => 'required|exists:users,password'
         ];
     }
