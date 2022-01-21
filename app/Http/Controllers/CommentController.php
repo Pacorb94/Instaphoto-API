8dce6fdafd65760f535cef73ab030707be9f3875
Pacorb94 <a@a.com>
2022-01-21 22:37:43 +0100

    CHANGE CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,67 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateOrUpdateCommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 use App\Models\Image;
-use Illuminate\Support\Facades\Validator;
-
 
 class CommentController extends Controller
 {
 
-    public function create($imageId, Request $request)
+    
+    public function create(Image $image, CreateOrUpdateCommentRequest $request)
     {
-        if ($imageId&&is_numeric($imageId)) {
-            $validator=$this->validations($request->all());
-            if ($validator->fails()) return response($validator->errors(), 400);
-            $image=Image::find($imageId);
-            //Si existe la imagen
-            if ($image) {
-                $comment=new Comment();
-                $comment->user_id=auth()->user()->id;
-                $comment->image_id=$imageId;
-                $comment->content=trim($request->input('content'));
-                $comment->save();
-                return response($comment, 201);
-            }
-            return response(['message'=>'No exists that image'], 404);
-        }
-        return response(['message'=>'Wrong image id'], 400);
+        $data = array_map('trim', $request->validated());
+        $comment = new Comment($data);
+        $comment->user_id = auth()->user()->id;
+        $comment->image_id = $image->id;
+        $comment->save();
+        return response(new CommentResource($comment), 201);
     }
 
-    public function delete($id)
+    public function update(Comment $comment, CreateOrUpdateCommentRequest $request)
     {
-        if ($id&&is_numeric($id)) {
-            $comment=Comment::find($id);
-            //Si existe el comentario
-            if ($comment) {
-                //Si el usuario es el que creó el comentario o el que creó la imagen (publicación)
-                if ($comment->user_id==auth()->user()->id
-                ||$comment->getImage->user_id==auth()->user()->id) {
-                    $comment->delete();
-                    return response(['message'=>'Comment deleted']);
-                }
-                return response(['message'=>'That user is not who created the comment or the image'], 500);
-            }
-            return response(['message'=>'No exists that comment'], 404);
-        }
-        return response(['message'=>'Wrong id'], 400);
+        $data = array_map('trim', $request->validated());
+        //Modificamos la columna updated_at
+        $comment->touch();
+        //Asignamos los campos
+        $comment->fill($data);
+        $comment->update();
+        return response(new CommentResource($comment));
     }
 
-    /**
-     * Función que hace la validación
-     * @param $request
-     * @return
-     */
-    public function validations($request)
+    public function delete(Comment $comment)
     {
-        $validator=Validator::make(
-            $request,
-            [
-                'content'=>'required'
-            ]
-        );
-        return $validator;
+        $image = Image::find($comment->image_id);
+        //Si el usuario es el que creó el comentario o el que creó la imagen
+        if (
+            $comment->user_id == auth()->user()->id
+            || $image->user_id == auth()->user()->id
+        ) {
+            $comment->delete();
+            return response(['message' => 'Deleted comment']);
+        }
+        return response(['message' => 'You didn´t create the comment or the image'], 400);
     }
 }
