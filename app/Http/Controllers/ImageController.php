ba8e927728abcb669cc1daf56162b5eef741e7b6
Pacorb94 <a@a.com>
2022-01-21 19:55:20 +0100

    CHANGE WIP ImageController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,34 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UpdateImageRequest;
-use App\Http\Requests\CreateImageRequest;
+use App\Http\Requests\CreateOrUpdateImageRequest;
 use App\Http\Resources\ImageCollection;
 use App\Http\Resources\ImageResource;
 use App\Models\Image;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File;
-
 
 class ImageController extends Controller
 {
 
+    public function __construct()
+    {
+        date_default_timezone_set('Europe/Madrid');
+    }
 
-    public function create(CreateImageRequest $request)
+    public function create(CreateOrUpdateImageRequest $request)
     {
         $data = $request->validated();
-        //Debemos configurar la fecha y tiempo
-        date_default_timezone_set('Europe/Madrid');
-        $imageName = date('d-m-Y_H-i-s') . '_' .
-            preg_replace('/\s+/', '_', $data['image']->getClientOriginalName());
-        //Almacenamos la imagen en la carpeta
-        Storage::disk('images')->put($imageName, File::get($data['image']));
         $image = new Image($data);
-        //TODO: qRuta de la imagen de la carpeta images
-        $image->image = $imageName;
+        $image->image = $this->moveImage($data['image']);
         $image->user_id = auth()->user()->id;
         $image->save();
         return response(new ImageResource($image), 201);
+    }
+
+    public function update(Image $image, CreateOrUpdateImageRequest $request)
+    {
+        $data = $request->validated();
+        //Si en la petición está la clave 'description'
+        if ($request->has('description')) {
+            $image->description = trim($data['description']);
+        }
+        //Borramos la antigua imagen
+        Storage::disk('images')->delete($image->image);
+        $image->image = $this->moveImage($data['image']);
+        //Modificamos la columna updated_at
+        $image->touch();
+        $image->update();
+        return response(new ImageResource($image));
+    }
+
+    private function moveImage($image)
+    {
+        $imageName = date('d-m-Y_H-i-s') . '_' .
+            preg_replace('/\s+/', '_', $image->getClientOriginalName());
+        $image->move(storage_path() . '\app\images\\', $imageName);
+        return $imageName;
     }
 
     public function getImages()
@@ -38,26 +56,21 @@ class ImageController extends Controller
         return response(new ImageCollection($images));
     }
 
-    public function update(Image $image, UpdateImageRequest $request)
-    {
-        $data = array_map('trim', $request->validated());
-        //Modificamos la columna updated_at
-        $image->touch();
-        //Con tap obtenemos la imagen modificada
-        $imageUpdated = tap($image)->update($data);
-        return response(new ImageResource($imageUpdated));
-    }
-
     public function delete(Image $image)
     {
-        if ($image->comments() > 0) {
-            foreach ($image->comments() as $comment) $comment->delete();
-        }
-        if ($image->likes() > 0) {
-            foreach ($image->likes() as $like) $like->delete();
-        }
+        //VER SI SE BORRAN CON ON CASCADE
+        // if (count($image->comments) > 0) {
+        //     foreach ($image->comments as $comment) {
+        //         $comment->delete();
+        //     }
+        // }
+        // if (count($image->likes) > 0) {
+        //     foreach ($image->likes as $like) {
+        //         $like->delete();
+        //     }
+        // }
         $image->delete();
-        //Borramos la imagen del Storage
+        //Borramos la imagen
         Storage::disk('images')->delete($image->image);
         return response(['message' => 'Image deleted']);
     }
