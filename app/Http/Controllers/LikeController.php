5aea8d8544a32fa46a4527a3a528c52fbf777adb
Pacorb94 <a@a.com>
2022-01-21 21:48:35 +0100

    CHANGE LikeController

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,61 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\LikeResource;
 use App\Models\Image;
 use App\Models\Like;
 
 class LikeController extends Controller
 {
-   
-    public function giveLike($imageId)
+
+    public function giveLike(Image $image)
     {
-        if ($imageId&&is_numeric($imageId)) {
-            $image=Image::find($imageId);
-            //Si existe la imagen
-            if ($image) {
-                $likeExists=Like::where('user_id', auth()->user()->id)
-                            ->where('image_id', $imageId)->count();
-                //Si no se le dió like
-                if ($likeExists==0) {                 
-                    $like=new Like();
-                    $like->user_id=auth()->user()->id;
-                    $like->image_id=$imageId;
-                    $like->save();
-                    return response($like, 201);
-                }
-                return response(['message'=>'You already liked the photo'], 500);
-            }
-            return response(['message'=>'No exists that image'], 404);
+        $likeByUser = Like::where('user_id', auth()->user()->id)
+            ->where('image_id', $image->id)->first();
+        //Si el usuario no le dió like
+        if (!$likeByUser) {
+            $like = new Like();
+            $like->user_id = auth()->user()->id;
+            $like->image_id = $image->id;
+            $like->save();
+            return response(new LikeResource($like), 201);
         }
-        return response(['message'=>'Wrong image id'], 400);
+        return response(['message' => 'You already liked the photo'], 400);
     }
 
-    public function getImagesUserLiked()
+    public function giveDislike(Image $image)
     {
-        $likes=Like::where('user_id', auth()->user()->id)->orderBy('id', 'desc')->paginate(5);
-        $images=[];
-        //Para obtener las imágenes con like deberemos recorrer los likes y acceder a la imagen
-        foreach ($likes as $like) array_push($images, $like->getImage);      
-        return response(['user'=>auth()->user(), 'images'=>$images]);
-    }
-
-    public function giveDislike($imageId)
-    {
-        if ($imageId&&is_numeric($imageId)) {
-            $image=Image::find($imageId);
-            //Si existe la imagen
-            if ($image) {
-                $like=Like::where('user_id', auth()->user()->id)
-                            ->where('image_id', $imageId)->first();
-                //Si se le dió like
-                if ($like) {
-                    $like->delete();
-                    return response(['message'=>'You dislike the photo']);
-                }
-                return response(['message'=>'You didnt like the photo'], 500);
-            }
-            return response(['message'=>'No exists that image'], 404);
+        $like = Like::where('user_id', auth()->user()->id)
+            ->where('image_id', $image->id)->first();
+        //Si el usuario le dió like
+        if ($like) {
+            $like->delete();
+            return response(['message' => 'You disliked the photo']);
         }
-        return response(['message'=>'Wrong image id'], 400);
+        return response(['message' => 'You have to like the photo first'], 400);
     }
 }
