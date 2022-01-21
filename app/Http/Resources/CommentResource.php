0212d93ea3c132b7f442895364edca5604d728b7
Pacorb94 <a@a.com>
2022-01-21 22:32:47 +0100

    ADD CommentResource

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'image_id' => $this->image_id,
+            'content' => $this->content,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'user' => new UserResource($this->user)
+        ];
+    }
+}
